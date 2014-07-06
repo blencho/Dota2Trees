aa49ff1207615e21d667f92d55ceacb702d885a4
Belai Lencho <blencho04@gmail.com>
2014-07-06 01:15:12 -0400

    renamed steamrequests module to just requests

--- dota2/steam/requests.py
+++ dota2/steam/requests.py
@@ -10,17 +10,15 @@ import time
 #from bs4 import BeautifulSoup
 #import nltk
 
-STEAM_URL = "http://api.steampowered.com/"
-KEY = 'F7DB115339A12F8A0D76AF7250A5B1AD'
-HERO_NAME_PREFIX = 'npc_dota_hero_'
-
 # Skill Levels
 ANY_SKILL = 0
 NORMAL_SKILL = 1
 HIGH_SKILL = 2
 VERY_HIGH_SKILL = 3
 
-
+_STEAM_URL = "http://api.steampowered.com/"
+_KEY = 'F7DB115339A12F8A0D76AF7250A5B1AD'
+_HERO_NAME_PREFIX = 'npc_dota_hero_'
 
 class SteamRequest(object):
     '''
@@ -30,18 +28,17 @@ class SteamRequest(object):
     requiredDict = {}
     specificationDict = {}
      
-
     def __init__(self):
         '''
         Constructor
         '''
         if not SteamRequest.heroDict:
-            SteamRequest.heroDict = self.getHeroes() # TODO: not sure if thread safe
+            SteamRequest.heroDict = self._get_heroes() # TODO: not sure if thread safe
         
     # Thought this might simplify things. Can change whether or not it should send requests
     def steam_request(self, domain, cmd, params):
-        url = STEAM_URL
-        url += domain + '/' + cmd + '/v1?key={0}'.format(KEY) + '&format=json'
+        url = _STEAM_URL
+        url += domain + '/' + cmd + '/v1?key={0}'.format(_KEY) + '&format=json'
         for param in params:
             url += '&{0}'.format(param)
         try:
@@ -52,14 +49,14 @@ class SteamRequest(object):
         json_data = json.load(data)
         return json_data
         
-    def getHeroes(self):
+    def _get_heroes(self):
         domain = 'IEconDOTA2_570'
         cmd = 'GetHeroes'
         tmpHeroDict = {}
         tmpJSON = self.steam_request(domain, cmd, [])
         heroList = tmpJSON['result']['heroes']
         for hero in heroList: 
-            tmpHeroDict[str(hero['id'])] = hero['name'][len(HERO_NAME_PREFIX):]
+            tmpHeroDict[str(hero['id'])] = hero['name'][len(_HERO_NAME_PREFIX):]
         return tmpHeroDict
     
     def setKey(self,key="4D7D7B6884092FAD9E59B6CAA572F588"):
@@ -76,6 +73,9 @@ class SteamRequest(object):
         
         self.requiredDict['primaryDomain'] = primaryDomain
 
+    def get_hero(self, id):
+        return SteamRequest.heroDict[str(id)]
+
     # Returns most recent, "Very High" skill games
     # Default matches returned is 25
     def get_recent_matches(self, nummatches=1, skill=NORMAL_SKILL, id=-1):
@@ -85,8 +85,8 @@ class SteamRequest(object):
             'skill={0}'.format(str(skill))]
         if id != -1:
             params.extend(['account_id={0}'.format(str(id))])
-        tmpJSON = self.steam_request(domain, cmd, params)
-        return tmpJSON['result']
+        tmp_json = self.steam_request(domain, cmd, params)
+        return tmp_json['result']
         
     """
     ======================================================================
